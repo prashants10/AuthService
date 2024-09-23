9fa9f60217a25234d130940ba246671c2ab8fb2c
Prashant Sharma <prashant.sharma@thoughts2binary.com>
2024-09-23 16:31:37 +0530

    Added get user by id in repo

--- src/repository/user-repository.js
+++ src/repository/user-repository.js
@@ -1,9 +1,21 @@
-const {User} = require("../models/index");
+const { User } = require("../models/index");
 
 class UserRepository {
   async createUser(data) {
     try {
       const user = await User.create(data);
+      return user;
+    } catch (err) {
+      console.log("Something went wrong on repository layer");
+      throw err;
+    }
+  }
+
+  async getUser(userId) {
+    try {
+      const user = await User.findByPk(userId, {
+        attributes: ["email", "id"],
+      });
       return user;
     } catch (err) {
       console.log("Something went wrong on repository layer");
